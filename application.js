816047eec8feb5d80542e7b04f7b25d83ab5a868
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-16 13:05:47 -0400

    Core Shopping 2.0  /application.js

--- application.js
+++ application.js
@@ -660,7 +660,7 @@ function renderPostDetails(container, template, collection){
     var template_html = $(template).html();
     $.each( collection , function( key, val ) {
         if (val.image_url.indexOf('missing.png') > -1) {
-            val.post_image = "http://assets.codecloudapp.com/sites/56056be06e6f641a1d020000/image/png/1446826281000/stc-logo-holiday-360 copy.png";
+            val.post_image = "http://assets.codecloudapp.com/sites/575838706e6f642b07060000/image/png/1461352407000/HallifaxLogo.png";
         } else {
             val.post_image = val.image_url;
         }
