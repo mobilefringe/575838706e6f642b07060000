ca66159b57422f77de03b64e5348907eb3d87720
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-01 14:16:59 -0400

    Core Shopping 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -62,11 +62,11 @@ function show_content(){
         if(v.is_holiday == true){
             var hours_day = new Date(v.holiday_date + "T07:00:00Z")
             if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
+                console.log(v)
                 var open_time = new Date (v.open_time);
                 var close_time = new Date (v.close_time);
                 v.open_time = convert_hour(open_time);
-                v.close_time = convert_hour(close_time);    
-                v.day = get_day(d.getDay())
+                v.close_time = convert_hour(close_time);
                 v.h = v.open_time+ " - " + v.close_time;
                 $('#hours_home').text(v.h)
             }
