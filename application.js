becbfe42b2e15a0b1334bb36352614969254c185
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-16 14:45:14 -0400

    Core Shopping 2.1  /application.js

--- application.js
+++ application.js
@@ -597,13 +597,14 @@ function renderHomeHours(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);    
     $.each( item_list , function( key, val ) {
+        var d = new Date();
         val.month = get_month(d.getMonth());
         val.weekday = addZero(d.getDate());
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = in_my_time_zone(moment(val.open_time), "hh:mm a");
             var close_time = in_my_time_zone(moment(val.close_time), "hh:mm a");
             val.h = open_time + " - " + close_time;
-            val.day = 
+            val.day = get_day(d.getDay())
         } else {
             val.h = "Closed";
         }
