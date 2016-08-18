4210f22cc34046aa6191f76d0f3294bea262e927
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-18 11:45:42 -0400

    Core Shopping 2.1  /application.js

--- application.js
+++ application.js
@@ -371,13 +371,7 @@ function renderJobs(container, template, collection){
             val.dates = start.format("MMM DD")
         }
         else{
-            val.var end = moment(val.end_date).tz(site_json.time_zone);
-        if (start.format("DMY") == end.format("DMY")){
-            val.dates = start.format("MMM DD")
-        }
-        else{
             val.dates = start.format("MMM DD") + " - " + end.format("MMM DD")
-        } = start.format("MMM DD") + " - " + end.format("MMM DD")
         }
         
         var rendered = Mustache.render(template_html,val);
