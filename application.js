7ca3ba1fa9f652e9086b87dd874a04b251ecb435
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-11 12:57:22 -0400

    Core Shopping 2.4  /application.js

--- application.js
+++ application.js
@@ -13,8 +13,15 @@ function renderStoreList(container, template, collection, starter, breaker){
         }
             
         //var categories = getStoreCategories();
+        if(val.categories != null){
+            try {
+                val.cat_list = val.categories.join(',');
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        
         var current_initial = val.name[0];
-        val.cat_list = val.categories.join(',')
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
             val.initial = "";
             val.show = "display:none;";
